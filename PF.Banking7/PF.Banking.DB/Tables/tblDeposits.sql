7915ec163b72be1e8e51b7f359e6e49bfa9d3ead
Brian D. Foote <foote@fvtc.edu>
2019-12-19 08:55:58 -0600

    Added primary key

--- PF.Banking7/PF.Banking.DB/Tables/tblDeposits.sql
+++ PF.Banking7/PF.Banking.DB/Tables/tblDeposits.sql
@@ -2,5 +2,6 @@
 	[Id] [int] NOT NULL,
 	[CustomerId] [int] NOT NULL,
 	[Amount] [decimal](18, 2) NOT NULL,
-	[TransDate] [datetime] NOT NULL
+	[TransDate] [datetime] NOT NULL, 
+    CONSTRAINT [PK_tblDeposits] PRIMARY KEY ([Id])
 )
